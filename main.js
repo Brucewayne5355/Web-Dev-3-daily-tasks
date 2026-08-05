e2388d94be439400255fa9a49d1750183eec9917
Ryan <115890972+Brucewayne5355@users.noreply.github.com>
2026-08-05 21:54:48 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+//  const des = [
+//    "delhi",
+//    "kerala",
+//    "odisha",
+//    "karnataka",
+//  ];
+
+
+
+
+const des = require('./data/des');
+console.log(des);
+    
+
+
+
+
+
+
+
+const packages = [
+    {
+        name: "package1",
+        version: "1.0.0.",
+    },
+    {
+        name: "package2",
+        version: "2.0.0."
+    }
+]
+module.exports = packages;
+
+
+//brucewayne5355
