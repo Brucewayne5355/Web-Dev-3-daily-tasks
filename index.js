d189a4ac98492c0f3d0b7b5c2a8ad7e57c212ba3
Ryan <115890972+Brucewayne5355@users.noreply.github.com>
2026-09-23 12:02:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ console.log(`Free Memory: ${os.freemem()} bytes`);
 console.log(`Total Memory: ${os.totalmem()} bytes`);
 console.log(`Home Directory: ${os.homedir()}`);
 console.log(`Temporary Directory: ${os.tmpdir()}`);
+
+
+
+
+console.log('Path Infomration:');
+console.log('Current Directory: ${__dirname}');
+const filePath = path.join(__dirname, 'example.txt');
+
+//create new file
+fstat.write('example.txt', 'Hello, World!', (err) =>{
+    if(err) throw err;
+    console.log('File has been written successfully.');
+});
